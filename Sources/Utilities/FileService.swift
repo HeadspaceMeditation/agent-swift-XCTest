60879b6520c6430de0d8e61663c4ef60b981a374
Natallia Mikulskaya <Natallia_Mikulskaya@epam.com>
2019-12-02 16:09:34 +0300

    minor fixes - newline character added

--- Sources/Utilities/FileService.swift
+++ Sources/Utilities/FileService.swift
@@ -12,7 +12,7 @@ final class FileService {
   private let fileManager = FileManager()
   private let fileExtention = ".log"
   private let logSubdirectoryName = "testLogs"
-  private let initialLogText = "Test log: "
+  private let initialLogText = "Test log: \r\n"
     
   private var logsDirectoryFullName : String
   private var logDirectoryURL : URL
